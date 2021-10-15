e8457ff6439fad75effd15da200be7ef1bc9fea3
fabiof2x <fabioar@gmail.com>
2021-10-15 16:37:28 -0300

    Selecionando uma tarefa

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,10 +1,31 @@
 import { ITarefa } from '../../../types/tarefa';
 import style from '../Lista.module.scss';
 
-export default function Item({ tarefa, tempo, selecionado, completado, id }: ITarefa) {
+interface Props extends ITarefa {
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
+
+export default function Item({
+  tarefa,
+  tempo,
+  selecionado,
+  completado,
+  id,
+  selecionaTarefa
+}: Props) {
   console.log(`Item atual: ${JSON.stringify({ tarefa, tempo, selecionado, completado, id })}`)
   return (
-    <li className={style.item}>
+    <li
+      className={style.item}
+      onClick={() => selecionaTarefa(
+        {
+          tarefa,
+          tempo,
+          selecionado,
+          completado,
+          id
+        }
+      )}>
       <h3>{tarefa}</h3>
       <span>{tempo}</span>
     </li>
